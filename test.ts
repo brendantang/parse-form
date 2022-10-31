a92af0edc88c7a03e2a6162317d828aaaa2233ae
Brendan Tang <b@brendantang.net>
2022-10-31 17:58:52 -0400

    map tests

--- test.ts
+++ test.ts
@@ -125,4 +125,43 @@ Deno.test("validators", async function parseTest(t) {
     assertFails("must be less than 29", chain(num, lessThan(29))("30"));
     assertFails("is not a number", chain(num, lessThan(29))("foo"));
   });
+  await t.step("mapping", async (t) => {
+    await t.step("map", () => {
+      function increment(n: number): number {
+        return n + 1;
+      }
+      assertSucceeds(2, map(increment, num)("1"));
+      assertFails("is not a number", map(increment, num)("x"));
+    });
+    await t.step("map2", () => {
+      function add(a: number, b: number): number {
+        return a + b;
+      }
+      assertSucceeds(2, map2(add, num, num)("1"));
+    });
+    await t.step("map3", () => {
+      function add(a: number, b: number, c: number): number {
+        return a + b + c;
+      }
+      assertSucceeds(3, map3(add, num, num, num)("1"));
+    });
+    await t.step("map4", () => {
+      function add(a: number, b: number, c: number, d: number): number {
+        return a + b + c + d;
+      }
+      assertSucceeds(4, map4(add, num, num, num, num)("1"));
+    });
+    await t.step("map5", () => {
+      function add(
+        a: number,
+        b: number,
+        c: number,
+        d: number,
+        e: number,
+      ): number {
+        return a + b + c + d + e;
+      }
+      assertSucceeds(5, map5(add, num, num, num, num, num)("1"));
+    });
+  });
 });
