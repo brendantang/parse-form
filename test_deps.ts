9a79d942bbfbe8c4e49f0cceff67409bab8745f5
Brendan Tang <b@brendantang.net>
2022-11-07 21:14:48 -0500

    Fix assertFails argument order

--- test_deps.ts
+++ test_deps.ts
@@ -10,5 +10,5 @@ export function assertFails<T>(
   reason: string,
   testCase: ValidationResult<T>,
 ) {
-  assertEquals(Result.Err({ reason }), testCase);
+  assertEquals(testCase, Result.Err({ reason }));
 }
